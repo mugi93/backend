68379ca24d9a67fc48177344024b1b7877a04aec
Mugi <g.mugi93140@gmail.com>
2021-06-16 15:32:17 +0200

    vali etape 1

--- chapitre_04/validation/vali.js
+++ chapitre_04/validation/vali.js
@@ -1,44 +1,60 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const expressHandlebars = require("express-handlebars");
+const express = require("express");
+const expressValidator = require("express-validator");
+const passwordValidator = require('password-validator');
+const { body, validationResult } = require('express-validator');
+
+
 const app = express();
-app.engine("handlebars", expressHandlebars());
-app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+app.use(express.json());
 
+app.post('/signup',
+    expressValidator.body("username").isLength(min = 4, max = 30),
+    // custom((value) => {
+    //     console.log("value:", value)
+    //     if (value.lenght > 4) {
+    //         return true
+    //     } else {
+    //         return false
+    //     }
 
+    expressValidator.body("email").isEmail(),
+    
+    expressValidator.body("age").is().digits()
 
+    //   expressValidator.body("password").custom((value) => {
+    //     var schema = new passwordValidator();
+    //     schema
+    //       .is().min(8) // Minimum length 8
+    //       .is().max(100) // Maximum length 100
+    //       .has().uppercase() // Must have uppercase letters
+    //       .has().lowercase() // Must have lowercase letters
+    //       .has().digits(2) // Must have at least 2 digits
+    //       .has().not().spaces() // Should not have spaces
 
-mongoose.connect("mongodb://localhost:27017/students", (err) => {
-    if (err) {
-        console.error(err)
-    } else {
-        console.log("I'm connected to the database");
-    }
-})
+    //     return schema.validate(value);
+    //   }),
+    , (req, res) => {
+        const errors = validationResult(req);
+        if (errors.isEmpty() === false) {
+            res.json({
+                errors: errors.array()
+            });
+            return;
+        } else {
+            res.json({
+                success: true,
+                message: 'test ok'
+            });
+        }
 
-const port = 8000
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-
-
-
-app.get("/", (req, res) => {
-    res.render("home", {
-        title: "Handlebars c'est nul",
-        img: 'https://restcountries.eu/data/fra.svg',
-        authors: ["Auteur 1", "Auteur 2", "Auteur 3"],
+       
     });
 
-})
 
 
-app.listen(port, () => {
-    console.log(`J'écoute des requêtes sur le port ${port}`);
-})
+
+
+app.listen(8000, () => {
+    console.log('Server started in port 8000');
+});
